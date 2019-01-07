663456e27fee22d39869bcb703f262a287a3e82b
Kosuke Komiya <kosuke.komiya@gmail.com>
2019-01-07 16:43:42 +0900

    Add nearby conditions for shortcut

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
-  enum state: { available: 1, deleted: 0 }
+  enum state: {available: 1, deleted: 0}
+
+  scope :within, ->(meter) {where("ST_DistanceSphere(location, 'POINT(33.589154 130.392993)') <= #{meter}")}
+  scope :nearby, -> {order("'POINT(33.589154 130.392993)'::geometry <-> location")}
 end
