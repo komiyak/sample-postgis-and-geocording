23e32135f769d5ad549ded3f5ab3f2bfd8ba2671
Kosuke Komiya <kosuke.komiya@gmail.com>
2019-01-08 18:40:31 +0900

    Add distance ratio

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   enum state: {available: 1, deleted: 0}
 
-  scope :within, ->(meter) {where("ST_DistanceSphere(location, 'POINT(33.589154 130.392993)') <= #{meter}")}
+  # 1 km
+  ONE_KM_LAT = 0.008983
+  ONE_KM_LON = 0.010966
+  DISTANCE_RATIO = (ONE_KM_LAT + ONE_KM_LON) / 2
+
+  scope :within, ->(user, km) { where("ST_DWithin(#{PostgisUtil.wkt_format(user.location.x, user.location.y)}, location, #{DISTANCE_RATIO * km})") }
   scope :nearby, ->(user) { order("users.location <-> #{PostgisUtil.wkt_format(user.location.x, user.location.y)}") }
 end
